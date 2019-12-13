448427cbdd649b0d64e5de3eb9de4bc88c86b8dc
Stacy <Anastasiy.yamnik@gmail.com>
2019-12-13 16:57:21 +0600

    edit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div>
       <div className="App">
+       { /*
       <svg width="1440" height="155" viewBox="0 0 1440 155" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_d)">
 <path d="M399.449 134.429C333.775 170.979 93.1186 119.199 -19 88.7405V0H1441.01C1443.29 54.7672 1478.07 134.429 1441.01 134.429C1388.56 134.429 481.543 88.7404 399.449 134.429Z" fill="#FF8000"/>
@@ -27,7 +28,7 @@ function App() {
 </filter>
 </defs>
 </svg>
-
+       */}
          <Menu />
          <Novelty />
          <Main />
