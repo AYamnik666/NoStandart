85c39018437cf67859a5134e1c50110f4043c1e2
Stacy <Anastasiy.yamnik@gmail.com>
2019-12-19 15:13:29 +0600

    edit

--- src/components/Alboms/Alboms.js
+++ src/components/Alboms/Alboms.js
@@ -7,10 +7,25 @@ import Tops from '../Tops/Tops';
 import Tracks from './../Tracks/Traks';
 import Novelty from './../Novelty/Novelty';
 
+import arrowl from '../../assets/images/arrowl.svg'  ;
+import arrowr from '../../assets/images/arrowr.svg';
+import {BrowserRouter, Route} from "react-router-dom";
+
+import Info from '../Info/Info';
+import {NavLink} from "react-router-dom";
+
 
 class Alboms extends Component {
 
     render() {
+
+
+
+        let onAlbomInfo = () => {
+            alert('показать инфо альбома');
+        }
+
+
         return (
 
 
@@ -24,13 +39,22 @@ class Alboms extends Component {
                  <img src={beauty} className='beauty-face-t-r' />
                  <img src={beauty} className='beauty-face-b-r' />
                  <img src={beauty} className='beauty-face-b-l' />
+<div>
+
+
+                 </div>
+                 <div className='albom-face-arrow-right'></div>
 
                 <div className='alboms-container-artist'>Chance The Rapper</div>
                 <div className='alboms-container-albom-name'>The Big Day</div>
 
+                <div><img src={arrowl} className='albom-face-arrow-left' />
                 <img src={albomFace} className='albom-face-image' />
+                <img src={arrowr} className='albom-face-arrow-right'  onClick={ onAlbomInfo } /><NavLink to="/alboms/info">Концерты</NavLink></div>
                 </div>
                 <Tracks />
+
+                <Route path='/alboms/info' component={Info}/>
             </div>
 
 
---
+++ src/components/Info/Info.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './_info.css';
+
+
+
+class News extends Component {
+
+    render() {
+        return (
+            <div className='news-container'>
+                <h3>Info</h3>
+
+
+
+            </div>
+        )
+    }
+}
+
+export default News;
